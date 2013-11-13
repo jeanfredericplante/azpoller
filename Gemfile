6462c9d829162a1bd0735b49bac5fbaf524c0659
JF Plante <jf.plante@hp.com>
2013-11-12 22:36:36 -0800

    screen scrapping from the source, using the frequently bought together field

--- Gemfile
+++ Gemfile
@@ -2,4 +2,4 @@ source 'https://rubygems.org'
 
 gem 'nokogiri', '~> 1.6.0'
 gem "selenium-webdriver", "~> 2.37.0"
-gem 'money'
+  
--- pull.rb
+++ pull.rb
@@ -1,20 +1,15 @@
 require 'rubygems'
 require 'open-uri'
 require 'nokogiri'
-require "selenium-webdriver"
-require 'money'
-
-driver = Selenium::WebDriver.for :safari
-
 
 url = "http://www.amazon.com/Logitech-915-000194-Harmony-Control-Smartphone/dp/B00BQ5RYI4/ref=sr_1_1?ie=UTF8&qid=1383536405&sr=8-1&keywords=harmony"
 
 begin
-  driver.navigate.to url
-  price = driver.find_element(:id, 'priceblock_ourprice')
-  price_decimal= price.text.gsub(/[^\d.,'-]/, '')         # remove anything that's not a number, potential thousands_separator, or minus sign
-  price_decimal.chop! if price_decimal.match(/[\.|,]$/) #if the number ends with punctuation, just throw it out.
-  puts price_cents = price_decimal.gsub(".","").to_i
+ doc = Nokogiri::HTML(open(url, &:read))
+ frequently_bought_together_div = JSON.parse(doc.xpath("//*[@id='fbt_item_data']").text)
+ price_in_cents = frequently_bought_together_div["itemData"][0]["buyingPrice"] * 100 # getting the first item of the frequently bought together array
+ 
+ return price_in_cents.to_i
 
 rescue Exception => e
   print e, "\n"
